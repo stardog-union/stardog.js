5e9e57bbe63b14b90be7dc654f66fbe0c25e0694
Edgar Rodriguez <edgar@clarkparsia.com>
2012-12-31 13:30:26 -0500

    Close #3: Adding reasoning test cases + data
    - Adding data for reasoning with basic class hierarchy, to be extended for more complex reasoning test cases
    - Added test cases description for reasoning queries using QL, RL and EL, using changes in commit af0e6ea0e7cccce5eaf1e0b4b56654f9cd910c69
    - Simple test cases for reasoning over the class hierarchy

--- load_test_data.sh
+++ load_test_data.sh
@@ -1,2 +1,7 @@
+# load main test data
 stardog-admin drop -n nodeDB
 stardog-admin create -n nodeDB -e data/api_tests.nt
+
+# load reasoning test data
+stardog-admin drop -n nodeDBReasoning
+stardog-admin create -n nodeDBReasoning -e data/reasoning/tbox.ttl data/reasoning/abox.ttl
