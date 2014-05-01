95458f766a1ecd524f98c6c948e015fedcd43419
Edgar Rodriguez <edgar@clarkparsia.com>
2014-05-01 11:13:41 -0400

    Using win-spawn to have a platform independent test script, tested to work on windows.

--- test.js
+++ test.js
@@ -1,20 +1,14 @@
 "use strict";
 
-var spawn = require("child_process").spawn,
+var spawn = require("win-spawn"),
     loadData, runTests,
-    shell,
-    cmd,
-    bash_script = "load_test_data.bat",
+    bat_script = "load_test_data.bat",
     shell_script = "./load_test_data.sh",
     win_run_test = "node node_modules/mocha/bin/mocha",
     unix_run_test = "node_modules/mocha/bin/mocha";
 
 if (process.platform === "win32" && process.env.SHELL === undefined) {
-    shell = process.env.COMSPEC || "cmd.exe";
-
-    cmd = "\"" + shell + "\"" + bash_script;
-    loadData = spawn(cmd, [], { stdio: "inherit" });
-
+    loadData = spawn(bat_script, [], { stdio: "inherit" });
     loadData.on("exit", function (code) {
         if (code !== 0) {
             process.stdout.write("There was an error loading the test data.\n");
@@ -22,7 +16,6 @@ if (process.platform === "win32" && process.env.SHELL === undefined) {
             runTests = spawn(win_run_test, ["--reporter", "spec", "test/*.spec.js"], { stdio: "inherit" });
         }
     });
-
 } else {
     loadData = spawn(shell_script, [], { stdio: "inherit" });
 
@@ -33,5 +26,4 @@ if (process.platform === "win32" && process.env.SHELL === undefined) {
             runTests = spawn(unix_run_test, ["--reporter", "spec", "test/*.spec.js"], { stdio: "inherit" });
         }
     });
-
 }
