2f9d8fec6af223135166545ae19e437ac0e2312a
Edgar Rodriguez <edgar@clarkparsia.com>
2015-02-04 11:59:42 -0500

    * Update load script to new cmd formats

--- load_test_data.sh
+++ load_test_data.sh
@@ -2,8 +2,8 @@
 
 # load main test data
 stardog-admin db drop -n nodeDB
-stardog-admin db create -n nodeDB --searchable data/api_tests.nt
+stardog-admin db create -n nodeDB -o search.enabled=true -- data/api_tests.nt
 
 # load reasoning test data
 stardog-admin db drop -n nodeDBReasoning
-stardog-admin db create -n nodeDBReasoning --searchable data/reasoning/tbox.ttl data/reasoning/abox.ttl
+stardog-admin db create -n nodeDBReasoning -o search.enabled=true -- data/reasoning/tbox.ttl data/reasoning/abox.ttl
