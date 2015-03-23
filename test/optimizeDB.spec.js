bea2a6b8a60d710db207304c0a660cf69b88971b
Edgar Rodriguez <edgar@clarkparsia.com>
2015-03-23 14:33:48 -0400

    * fix test case for optimizing a db for stardog 3.0

--- test/optimizeDB.spec.js
+++ test/optimizeDB.spec.js
@@ -1,3 +1,5 @@
+var util = require('util');
+
 (function (root, factory) {
     "use strict";
 
@@ -32,7 +34,7 @@
         });
 
         it ("should get NOT_FOUND status code trying to optimize a non-existent DB.", function(done) {
-                
+
             conn.optimizeDB({ database: "nodeDB_optimize" }, function (data, response) {
 
                 expect(response.statusCode).to.be(404);
@@ -40,7 +42,7 @@
             });
         });
 
-        it ("should optimize an offline DB", function(done) {
+        it ("should optimize an online DB", function(done) {
 
             // Clean everything
             conn.dropDB({ database: "nodeDB_optimize" }, function () {
@@ -51,18 +53,23 @@
                     conn.copyDB({ dbsource: "nodeDB", dbtarget: "nodeDB_optimize" }, function (data, response2) {
                         expect(response2.statusCode).to.be(200);
 
-                        conn.optimizeDB({ database: "nodeDB_optimize" }, function (data, response3) {
+                        conn.onlineDB({ database: "nodeDB_optimize", strategy: "NO_WAIT" }, function (data, response3) {
                             expect(response3.statusCode).to.be(200);
 
-                            // Clean everything
-                            conn.dropDB({ database: "nodeDB_optimize" }, function (data, response4) {
+                            conn.optimizeDB({ database: "nodeDB_optimize" }, function (data, response4) {
+                                expect(data.message).to.be('nodeDB_optimize was successfully optimized.');
                                 expect(response4.statusCode).to.be(200);
 
-                                expect(conn).not.to.be(null);
-
-                                conn.onlineDB({ database: "nodeDB", strategy: "NO_WAIT" }, function (data, response5) {
+                                // Clean everything
+                                conn.dropDB({ database: "nodeDB_optimize" }, function (data, response5) {
                                     expect(response5.statusCode).to.be(200);
-                                    done();
+
+                                    expect(conn).not.to.be(null);
+
+                                    conn.onlineDB({ database: "nodeDB", strategy: "NO_WAIT" }, function (data, response6) {
+                                        expect(response6.statusCode).to.be(200);
+                                        done();
+                                    });
                                 });
                             });
                         });
