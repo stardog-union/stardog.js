d32afed86b088ca54ef6f3cd0a8e8d3bb19ef7ed
Jason Rogers <jason@stardog.com>
2019-11-21 10:17:44 -0500

    Better docs, too

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -697,6 +697,14 @@ declare namespace Stardog {
         }
 
         interface AdditionalHandlers {
+            /**
+             * Specify this method if you wish to act on the HTTP response
+             * immediately, before any further stardog.js processing.
+             * NOTE: The stardog.js processing will continue, unless
+             * `onResponseStart` explicitly returns `false`.
+             * 
+             * @param {Response} res HTTP response object.
+             */
             onResponseStart(res: Response): boolean | void;
         }
 
