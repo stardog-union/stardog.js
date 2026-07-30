c196850096b85573888325ada7a1a4436f99541e
anneeb <annee.barrett@stardog.com>
2026-07-30 10:29:34 -0400

    use expect.objectContaining

--- test/getDBOptions.spec.js
+++ test/getDBOptions.spec.js
@@ -30,9 +30,11 @@ describe('options.get()', () => {
     options.get(conn, database).then(res => {
       expect(res.status).toEqual(200);
       expect(typeof res.body).toEqual('object');
-      expect(res.body).toMatchObject({
-        'index.type': 'Disk',
-      });
+      expect(res.body).toMatchObject(
+        expect.objectContaining({
+          'index.type': 'Disk',
+        })
+      );
     }));
 });
 
@@ -50,9 +52,11 @@ describe('options.getAll()', () => {
     options.getAll(conn, database).then(res => {
       expect(res.status).toEqual(200);
       expect(typeof res.body).toEqual('object');
-      expect(res.body).toMatchObject({
-        'index.type': 'Disk',
-      });
+      expect(res.body).toMatchObject(
+        expect.objectContaining({
+          'index.type': 'Disk',
+        })
+      );
     }));
 });
 
@@ -65,8 +69,10 @@ describe('options.getAvailable', () => {
     options.getAvailable(conn).then(res => {
       expect(res.status).toEqual(200);
       expect(typeof res.body).toEqual('object');
-      expect(res.body).toMatchObject({
-        'database.online': {},
-      });
+      expect(res.body).toMatchObject(
+        expect.objectContaining({
+          'database.online': expect.any(Object),
+        })
+      );
     }));
 });
